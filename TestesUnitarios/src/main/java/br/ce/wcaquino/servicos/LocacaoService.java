1cb30a3fbc1ea436d170bbee6d096073823d1506
Samla Azevedo Tavares <samla_azevedo@outlook.com>
2021-12-13 21:15:44 -0300

    Initial commit

---
+++ TestesUnitarios/src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
@@ -0,0 +1,47 @@
+package br.ce.wcaquino.servicos;
+
+import static br.ce.wcaquino.utils.DataUtils.adicionarDias;
+
+import java.util.Date;
+
+import br.ce.wcaquino.entidades.Filme;
+import br.ce.wcaquino.entidades.Locacao;
+import br.ce.wcaquino.entidades.Usuario;
+import br.ce.wcaquino.utils.DataUtils;
+
+public class LocacaoService {
+	
+	public Locacao alugarFilme(Usuario usuario, Filme filme) {
+		Locacao locacao = new Locacao();
+		locacao.setFilme(filme);
+		locacao.setUsuario(usuario);
+		locacao.setDataLocacao(new Date());
+		locacao.setValor(filme.getPrecoLocacao());
+
+		//Entrega no dia seguinte
+		Date dataEntrega = new Date();
+		dataEntrega = adicionarDias(dataEntrega, 1);
+		locacao.setDataRetorno(dataEntrega);
+		
+		//Salvando a locacao...	
+		//TODO adicionar método para salvar
+		
+		
+		return locacao;
+	}
+
+	public static void main(String[] args) {
+		//arrange
+		LocacaoService service = new LocacaoService();
+		Usuario usuario = new Usuario("Samla");
+		Filme filme = new Filme("Pride and Prejudice", 1, 10.0);
+		
+		//act
+		Locacao locacao = service.alugarFilme(usuario, filme);
+		
+		//assert
+		System.out.println(locacao.getValor() == 10.0);
+		System.out.println(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
+		System.out.println(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
+	}
+}
