4f0bb1dcc45dba9adfe211397000151530eb613c
Samla Azevedo Tavares <samla_azevedo@outlook.com>
2022-01-24 17:02:45 -0300

    Using Spy

--- TestesUnitarios/src/test/java/br/ce/servicos/CalculadoraMockTest.java
+++ TestesUnitarios/src/test/java/br/ce/servicos/CalculadoraMockTest.java
@@ -1,14 +1,38 @@
 package br.ce.servicos;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
 
 
 
 public class CalculadoraMockTest {
-
+	
+	@Mock
+	private Calculadora calculadoraMock;
+	
+	@Spy
+	private Calculadora calculadoraSpy;
+	
+	@Before
+	public void before() {
+		MockitoAnnotations.initMocks(this);
+	}
+	
+	@Test
+	public void devoMostrarDiferencaEntreMockESpy() {
+		Mockito.when(calculadoraMock.somar(1, 2)).thenReturn(8);
+		Mockito.when(calculadoraSpy.somar(1, 2)).thenReturn(8);
+		
+		System.out.println("Mock: " + calculadoraMock.somar(1, 2));
+		System.out.println("Spy: " + calculadoraSpy.somar(1, 3));
+	}
+	
 	@Test
 	public void teste() { //exemplo de utilização de matchers e do mockito em uma classe concreta
 		Calculadora calc = Mockito.mock(Calculadora.class);
